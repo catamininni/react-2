664ae1d6bc48a96ee62e5e6a69b2972769bb92f0
catamininni <catamininni@gmail.com>
2021-05-30 17:20:12 -0300

    proyecto reactjs

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './Main.css';
+import NavBar from '../NavBar/Navbar'
+function Main() {
+  return (
+    <div className="App">
+      <NavBar/>
+
+    </div>
+
+
+  );
+}
+
+export default Main;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import './NavBar.css';
+
+function NavBar() {
+    return(
+        <nav class="topnav">
+                <a href="#section1">Sección 1</a>
+                <a href="#section2">Sección 2</a>
+        </nav>
+    );
+}
+
+export default NavBar;
