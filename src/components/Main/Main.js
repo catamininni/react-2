4fb68533e5176d4971566f924edc3943a2848ac5
catamininni <catamininni@gmail.com>
2021-05-30 17:47:42 -0300

    proyecto react.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
+
+import NavBar from '../NavBar/NavBar.js';
 import './Main.css';
-import NavBar from '../NavBar/Navbar'
 function Main() {
   return (
     <div className="App">
       <NavBar/>
-
     </div>
-
-
   );
 }
-
 export default Main;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,6 +3,7 @@ import './NavBar.css';
 function NavBar() {
     return(
         <nav class="topnav">
+                <div class='logo' ><img src='images/logo.png' alt='' width='60px' height='50px'/></div>
                 <a href="#section1">Sección 1</a>
                 <a href="#section2">Sección 2</a>
         </nav>
